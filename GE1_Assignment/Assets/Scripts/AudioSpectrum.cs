26e9c65489090f198c6e506dcba26ba1966ec7d8
pHoran17 <c16416742@mydit.ie>
2019-11-29 13:24:44 +0000

    Updated audioSpectrum class, added create cubes class with relevant objects and audioListener

---
+++ GE1_Assignment/Assets/Scripts/AudioSpectrum.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+public class AudioSpectrum : MonoBehaviour
+{
+    public float[] audSpec = new float[512];
+    AudioSource aSource;
+    //public static float specValue { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //audSpec = new float[512];
+        aSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //AudioListener.GetSpectrumData();
+        /*if (audSpec != null && audSpec.Length > 0)
+        {
+            specValue = audSpec[0] * 100;//100 used to normalize spectrum data
+        }
+        */
+        getSpectrumAudioSource();
+    }
+    void getSpectrumAudioSource()
+    {
+        aSource.GetSpectrumData(audSpec, 0, FFTWindow.Blackman);
+    }
+}
---
+++ GE1_Assignment/Assets/Scripts/CreateCubes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCubes : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject[] cubeArray = new GameObject[512];
+    public float maxScale;
+    public AudioSpectrum audioSpect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject sampleCube = (GameObject)Instantiate(cubePrefab);
+            sampleCube.transform.position = this.transform.position;
+            sampleCube.transform.parent = this.transform;
+            sampleCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);//Y value comes from 360/512
+            sampleCube.transform.position = Vector3.forward * 100;
+            cubeArray[i] = sampleCube;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if (cubeArray != null)
+            {
+                cubeArray[i].transform.localScale = new Vector3(10, (audioSpect.audSpec[i] * maxScale) + 2, 10);
+            }
+        }
+    }
+}
