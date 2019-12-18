e58c72c076ad77fe4da48439a0a648f749cd03f2
pHoran17 <c16416742@mydit.ie>
2019-12-18 00:54:58 +0000

    Added CreateCeilingCubes class with relevent GameObjects

---
+++ GE1_Assignment/Assets/Scripts/CreateCeilingCubes.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCeilingCubes : MonoBehaviour
+{
+    //GameObject[] objects;
+    List<GameObject> objects;
+    public GameObject prefab;
+    public AudioHandler audioH;
+    public float startScale;
+    public bool useBuffer;
+    float specLength;
+    int band = 4;
+    int height = 25;
+    int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objects = new List<GameObject>();
+        specLength = audioH.spectrum.Length;
+        //Create array of shapes here
+        for(int i = -21; i < 21; i++)
+        {
+            for (int j = -12; j < 12; j++)
+            {
+                Vector3 pos = new Vector3(i,height,j) * 10;
+                pos = transform.TransformPoint(pos);
+                GameObject go = GameObject.Instantiate<GameObject>(prefab, pos, Quaternion.identity);
+                go.transform.parent = this.transform;
+                go.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.value, 1, 5);
+                objects.Add(go);
+                
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Update transforms here
+         index = 0;
+        if (useBuffer)
+        {
+            foreach (GameObject g in objects)
+            {
+                g.transform.localScale = new Vector3(transform.localScale.x, audioH.bandBuffer[band] * startScale, transform.localScale.z);
+                index++;
+            }
+        }
+        if(!useBuffer)
+        {
+            foreach (GameObject g in objects)
+            {
+                g.transform.localScale = new Vector3(transform.localScale.x, audioH.spectrum[band] * startScale, transform.localScale.z);
+                index++;
+            }
+        }
+         
+    }
+}
